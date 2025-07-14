853812c5fc677a2d489ddef1f522db7f36723c0a
Gustavo Paes <gustavo910@gmail.com>
2025-07-14 17:41:18 -0300

    feat: auth by github in progress

--- src/main/java/com/ajudaqui/authenticationms/controller/LoginGithubController.java
+++ src/main/java/com/ajudaqui/authenticationms/controller/LoginGithubController.java
@@ -1,17 +1,20 @@
 package com.ajudaqui.authenticationms.controller;
+
 import com.ajudaqui.authenticationms.service.LoginGithubService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("login/github")
+@RequestMapping("/login/github")
 public class LoginGithubController {
-// verficar se a rota esta liberada para ser chamada
+  // verficar se a rota esta liberada para ser chamada
 
   @Autowired
   private LoginGithubService loginGithubService;
@@ -24,4 +27,11 @@ public class LoginGithubController {
     return new ResponseEntity<>(headers, HttpStatus.FOUND); // tem que devolver um code 302
   }
 
+  @GetMapping("/autorizado")
+  public ResponseEntity<String> obterToken(@RequestParam String code) {
+
+    // eu chamo o de cima.. com a uri que constri, e ele chama esse aqui, me passand os dads de auth do cliente
+    String token = loginGithubService.obterToken(code);
+    return ResponseEntity.ok(token);
+  }
 }
--- src/main/java/com/ajudaqui/authenticationms/service/LoginGithubService.java
+++ src/main/java/com/ajudaqui/authenticationms/service/LoginGithubService.java
@@ -1,17 +1,42 @@
 package com.ajudaqui.authenticationms.service;
 
 import java.net.URI;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import software.amazon.awssdk.services.sqs.endpoints.internal.Value.Str;
 
 @Service
 public class LoginGithubService {
 
   @Value("${oauth.github.client-id}")
   private String clientId = "";
+
+  @Value("${oauth.github.client-secret}")
+  private String clientSecret = "";
+
   @Value("${oauth.github.redirect-uri}")
   private String redirectUri = "";
+
+  // @Autowired
+  private final RestTemplate restTemplate;
+
+  public LoginGithubService(RestTemplateBuilder build) {
+    this.restTemplate = build.build();
+  }
 
   public URI factorUri() {
     String urlGithub = "https://github.com/login/oauth/authorize";
@@ -19,6 +44,25 @@ public class LoginGithubService {
     String scope = "&scope=read:user,user:email"; // Quais informações do usuário pegar...
     System.out.println("params " + params);
     return URI.create(urlGithub + params + scope);
+  }
+
+  public String obterToken(String authorization_code) {
+    String url = "https://github.com/login/oauth/access_token";
+
+    Map<String, String> body = new HashMap<>();
+    body.put("code", authorization_code);
+    body.put("client_id", clientId);
+    body.put("client_secret", clientSecret);
+    body.put("client_secret", clientSecret);
+
+    Arrays.asList("");
+    HttpHeaders httpHeaders = new HttpHeaders();
+    httpHeaders.setContentType(MediaType.APPLICATION_JSON); // Enviar o conteudo tipo json
+    httpHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON)); // espero receber tipo json
+
+    HttpEntity<Map<String, String>> request = new HttpEntity<>(body, httpHeaders);
+
+    return restTemplate.postForEntity(url, request, String.class).getBody();
   }
 
 }
